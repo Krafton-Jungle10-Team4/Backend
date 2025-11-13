6356e000ebed70a460ae3a182becbcd3a56d3ba2
hyeondxo <eun041212@gmail.com>
2025-11-14 01:48:29 +0900

    feat: update alembic version

---
+++ alembic/versions/9850cdd58948_merge_workflow_v2_and_document_.py
@@ -0,0 +1,24 @@
+"""merge workflow v2 and document embeddings branches
+
+Revision ID: 9850cdd58948
+Revises: e4f5g6h7i8j9, j9k0l1m2n3o4
+Create Date: 2025-11-13 16:02:11.871323
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '9850cdd58948'
+down_revision = ('e4f5g6h7i8j9', 'j9k0l1m2n3o4')
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass
